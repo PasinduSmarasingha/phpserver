e4480f926dfdb248a2eb31945cac11ffcaa09f01
Pasindu Samarasingha <61085855+TechKnowLK@users.noreply.github.com>
2023-10-17 23:26:40 +0530

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -4,14 +4,16 @@ apt upgrade
 
 pkg install php mariadb -y
 
-chmod +x startsql.sh && chmod +x startphp.sh
+chmod +x startsql.sh && chmod +x startphp.sh && chmod +x stopall.sh
 
 mv startphp.sh ../
 
 mv startsql.sh ../
 
+mv stopall.sh ../
+
 termux-setup-storage
 
-cd /sdcard
+cd && cd /sdcard
 
 mkdir php
